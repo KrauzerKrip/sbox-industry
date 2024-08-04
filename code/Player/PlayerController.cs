36bf6a503ab46d33b9e4d6037ec586d01b3c4c00
KrauzerKrip <krauzerkrip@gmail.com>
2024-08-04 13:23:43 +0300

    Add project files.

---
+++ code/Player/PlayerController.cs
@@ -0,0 +1,91 @@
+using Sandbox;
+using Sandbox.Citizen;
+
+public sealed class PlayerController : Component
+{
+	[Property]
+	[Category( "Components" )]
+	public GameObject Camera {  get; set; }
+	[Property]
+	[Category( "Components" )]
+	public CharacterController CharacterController { get; set; }
+	[Property]
+	[Category( "Components" )]
+	public CitizenAnimationHelper CitizenAnimationHelper { get; set; }
+	[Property]
+	[Category( "Stats" )]
+	[Range(0f, 400f, 1f)]
+	public float WalkSpeed { get; set; } = 120f;
+	[Property]
+	[Category( "Stats" )]
+	[Range(0f, 800f, 1f)]
+	public float RunSpeed { get; set; } = 250f;
+	[Property]
+	[Category( "Stats" )]
+	[Range(0f, 1000f, 1f)]
+	public float JumpStrength { get; set; } = 400f;
+
+	public Angles EyeAngles { get; set; }
+
+	protected override void OnUpdate()
+	{
+		EyeAngles += Input.AnalogLook;
+		Transform.Rotation = Rotation.FromYaw( EyeAngles.yaw );
+		Camera.Transform.Rotation = Rotation.FromYaw( EyeAngles.yaw ) + Rotation.FromPitch( EyeAngles.pitch );
+	}
+
+	protected override void OnFixedUpdate()
+	{
+		var wishVelocity = Input.AnalogMove * WalkSpeed * Transform.Rotation;
+
+		CharacterController.Accelerate( wishVelocity );
+		if ( CharacterController.IsOnGround )
+		{
+			CharacterController.ApplyFriction( 5f );
+
+			if (Input.Pressed("Jump"))
+			{
+				CharacterController.Punch( Vector3.Up * JumpStrength );
+
+				CitizenAnimationHelper.TriggerJump();
+			}
+		}
+		else
+		{
+			CharacterController.Velocity += Scene.PhysicsWorld.Gravity * Time.Delta;
+		}
+
+		CharacterController.Move();
+
+		CitizenAnimationHelper.IsGrounded = CharacterController.IsOnGround;
+		CitizenAnimationHelper.WithVelocity( CharacterController.Velocity );
+
+		base.OnFixedUpdate();
+	}
+
+	protected override void OnStart()
+	{
+		if (Components.TryGet<SkinnedModelRenderer>(out var model))
+		{
+			var clothing = ClothingContainer.CreateFromLocalUser();
+			clothing.Apply( model );
+		}
+
+		base.OnStart();
+	}
+
+	protected override void OnEnabled()
+	{
+		base.OnEnabled();
+	}
+
+	protected override void OnDisabled()
+	{
+		base.OnDisabled();
+	}
+
+	protected override void OnDestroy()
+	{
+		base.OnDestroy();
+	}
+}
